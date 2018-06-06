1eef4a0326ed1597a65ec11696d9c04d0e67e4fe
tigorini <mr7igrik@gmail.com>
2018-06-07 00:27:59 +0300

    usyo

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from flask import render_template
+from flask import url_for
+from flask import jsonify
 from pymongo import MongoClient
 
 app = Flask(__name__)
+app.config.from_pyfile('config.py')
 
-url_for('static/js', filename='index.js')
 
 client = MongoClient()
 posts = client.forum_inf.posts
@@ -12,4 +15,35 @@ topics = client.forum_inf.topics
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', topic='{{topic}}')
+
+
+@app.route('/get_topics')
+def get_topics():
+    data = []
+    for topic in topics.find():
+        data.append(topic['name'])
+    return jsonify(data)
+
+
+@app.route('/get_posts/<topic>')
+def get_posts(topic):
+    data = {}
+    data['topic'] = topic
+
+    tposts = posts.find({'topic': topic})
+    authors = []
+    data['posts'] = []
+    for post in tposts:
+        data['posts'].append({
+            'author': post['author'],
+            'datetime': post['datetime'],
+            'text': post['text'] 
+        })
+        if post['author'] not in authors:
+            authors.append(post['author'])
+    
+    data['authors'] = []
+    for auth in authors:
+        data['authors'].append([auth, posts.find({'topic': topic,'author': auth}).count()])
+    return jsonify(data)
---
+++ static/js/index.js
@@ -0,0 +1,132 @@
+google.charts.load('current', {'packages':['corechart']})
+
+
+var forum = new Vue({
+    el: '#content',
+    data: {
+        topic: '',
+        topics: []
+    },
+    created () {
+        $.get('/get_topics', function(json){
+            setTopics(json)
+            autocomplete(document.getElementById("search"), forum.topics)
+        })
+    },
+    methods:{
+        getTopicPosts: function(event){
+            event.preventDefault()
+            $.get('/get_posts/' + this.topic, function(json){
+                if (json.posts.length == 0){
+                    $('#piechart').text('No results')
+                    return
+                }
+                var chartData = json.authors
+                chartData.splice(0, 0, ['Author', 'Posts'])
+        
+                var options = {
+                    'title': json.topic,
+                    'width': 800,
+                    'height': 500,
+                    'pieHole': 0.4,
+                    'backgroundColor': '#e6e6e6'
+                }
+                drawChart(chartData, options)
+            })
+        }
+    }
+})
+
+
+function setTopics(json){
+    forum.topics = forum.topics.concat(json)
+}
+
+function setTopic(data){
+    forum.topic = data
+}
+
+function drawChart(dataChart, options){
+    var data = google.visualization.arrayToDataTable(dataChart)
+    var chart = new google.visualization.PieChart(document.getElementById('piechart'))
+    chart.draw(data, options)
+}
+
+
+function autocomplete(inp, topics) {
+    var currentFocus
+
+    inp.addEventListener("input", function(e) {
+        var a, b, i, val = this.value
+
+        closeAllLists()
+        if (!val) { return false;}
+        currentFocus = -1
+      
+        a = document.createElement("DIV")
+        a.setAttribute("id", this.id + "autocomplete-list")
+        a.setAttribute("class", "autocomplete-items")
+        this.parentNode.appendChild(a)
+        
+        for (i = 0; i < topics.length; i++) {
+            if (topics[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+                b = document.createElement("DIV")
+                b.innerHTML = "<strong>" + topics[i].substr(0, val.length) + "</strong>"
+                b.innerHTML += topics[i].substr(val.length);
+                b.innerHTML += "<input type='hidden' value='" + topics[i] + "'>"
+
+                b.addEventListener("click", function(e) { 
+                    setTopic(this.getElementsByTagName("input")[0].value)
+                    closeAllLists()
+                })
+                a.appendChild(b)
+            }
+        }
+    })
+ 
+    inp.addEventListener("keydown", function(e) {
+        var x = document.getElementById(this.id + "autocomplete-list")
+        if (x) x = x.getElementsByTagName("div")
+        if (e.keyCode == 40) {
+            currentFocus++
+            addActive(x)
+        } else if (e.keyCode == 38) {
+            currentFocus--
+            addActive(x)
+        } else if (e.keyCode == 13) {
+            e.preventDefault()
+            if (currentFocus > -1) {
+                if (x) x[currentFocus].click()
+            } else {
+                forum.getTopicPosts(e)
+                closeAllLists()
+            }
+        }
+    })
+  
+    function addActive(x) {
+        if (!x) return false
+        removeActive(x)
+
+        if (currentFocus >= x.length) currentFocus = 0;
+        if (currentFocus < 0) currentFocus = (x.length - 1)
+        x[currentFocus].classList.add("autocomplete-active")
+    }
+    function removeActive(x) {
+        for (var i = 0; i < x.length; i++) {
+            x[i].classList.remove("autocomplete-active")
+        }
+    }
+    function closeAllLists(elmnt) {
+        var x = document.getElementsByClassName("autocomplete-items");
+        for (var i = 0; i < x.length; i++) {
+            if (elmnt != x[i] && elmnt != inp) {
+                x[i].parentNode.removeChild(x[i])
+            }
+        }
+    }
+
+    document.addEventListener("click", function (e) {
+        closeAllLists(e.target)
+    });
+}
